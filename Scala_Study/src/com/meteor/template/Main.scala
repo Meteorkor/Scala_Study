c58ea26ff1be5d8ce61617c23472190afe9035c1
kim <kim@kim-PC>
2014-10-04 14:04:29 +0900

    2014-10-04

---
+++ Scala_Study/src/com/meteor/template/Main.scala
@@ -0,0 +1,24 @@
+package com.meteor.template
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    
+    println("Hello World~!");
+    
+    
+    /**
+     * val : JAVA final or const
+     * var : variable
+     * 
+     */
+    
+    var ob = new Sample("test")
+    
+    ob = new Sample("sss")
+    
+    println( "ob : " + ob );
+    
+  }
+
+}
---
+++ Scala_Study/src/com/meteor/template/Sample.scala
@@ -0,0 +1,6 @@
+package com.meteor.template
+
+class Sample(var name:String="kim", var age:Integer=0) {
+  
+  
+}
